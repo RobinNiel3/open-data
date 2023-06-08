e1d34a0d93e84e1745d3cc9102812180c600612f
Robin Niel <robin.niel@brut.media>
2023-06-08 13:43:05 +0200

    Push

--- firestore/get_item.py
+++ firestore/get_item.py
@@ -1,15 +1,9 @@
-from google.cloud import firestore
-
-# Instantiation du client
-db = firestore.Client(project="brt-svc-data-platform-dev")
-
-# Création de la référence au document
-doc_ref = db.collection("sources").document("0cK5UJflqKHJkVoCOhcJ")
-
-# Récupération du document
-doc = doc_ref.get()
-
-if doc.exists:
-    print(f"Document data: {doc.to_dict()}")
-else:
-    print("No such document!")
+def get_item(db_client, collection_name, key):
+    doc_ref = db_client.collection(collection_name).document(key)
+    doc = doc_ref.get()
+    if doc.exists:
+        print(f"Document data: {doc.to_dict()}")
+        return doc.to_dict()
+    else:
+        print("No such document!")
+        return None
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from google.cloud import firestore
+from firestore.get_item import get_item
 
 general_pages_router = APIRouter()
 
+db = firestore.Client(project="brt-svc-data-platform-dev")
 
-@general_pages_router.get("/test")
-async def home(request: Request):
+@general_pages_router.get("/source/{item_id}")
+async def home(item_id: str, request: Request):
     templates = Jinja2Templates(directory="templates")
     return templates.TemplateResponse("interface.html", {"request": request,
                                                          "video_title": "Video title",
