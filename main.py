e1d7c7a819be3c7f712bf4ba3e0c0bc9f6aa78f0
Robin Niel <robin.niel@brut.media>
2023-06-08 15:07:55 +0200

    Firestore

--- main.py
+++ main.py
@@ -9,10 +9,20 @@ general_pages_router = APIRouter()
 
 db = firestore.Client(project="brt-svc-data-platform-dev")
 
+def get_sources_hypertext(sources):
+    final_str = "<ul>"
+    for source in sources:
+        final_str += f'<li><a href="{source}">{source}</a></li>'
+    final_str += "</ul>"
+    return final_str
+
 @general_pages_router.get("/source/{item_id}")
 async def home(item_id: str, request: Request):
     templates = Jinja2Templates(directory="templates")
+    print(item_id)
+    item = get_item(db, "sources", item_id)
+    print(item)
     return templates.TemplateResponse("interface.html", {"request": request,
-                                                         "video_title": "Video title",
-                                                         "page_title": "Page Title",
-                                                         "description": "Video description"})
+                                                         "video_title": item["title"],
+                                                         "page_title": item["title"],
+                                                         "description": get_sources_hypertext(item["sources"])})
