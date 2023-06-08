73835631aea5e727579582e0ca9bb94cde1c860b
Robin Niel <robin.niel@brut.media>
2023-06-08 13:22:16 +0200

    test

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
 general_pages_router = APIRouter()
 
 
 @general_pages_router.get("/test")
 async def home(request: Request):
+    templates = Jinja2Templates(directory="templates")
     return templates.TemplateResponse("interface.html", {"request": request,
-                                                         "title": "Video title",
+                                                         "video_title": "Video title",
                                                          "page_title": "Page Title",
                                                          "description": "Video description"})
