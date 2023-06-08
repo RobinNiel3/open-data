c718064b20dcb591c92edb5db1447f98c82affb8
Robin Niel <robin.niel@brut.media>
2023-06-08 16:34:39 +0200

    Safe html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -12,17 +12,18 @@ db = firestore.Client(project="brt-svc-data-platform-dev")
 def get_sources_hypertext(sources):
     final_str = "<ul>"
     for source in sources:
-        final_str += f'<li><a href="{source}">{source}</a></li>'
+        final_str += f'<li><a href="{source["url"]}">{source["label"]}</a></li>'
     final_str += "</ul>"
     return final_str
 
 @general_pages_router.get("/source/{item_id}")
 async def home(item_id: str, request: Request):
     templates = Jinja2Templates(directory="templates")
-    print(item_id)
     item = get_item(db, "sources", item_id)
-    print(item)
-    return templates.TemplateResponse("interface.html", {"request": request,
+    if item is None :
+        return HTTPException(status_code=404, detail="Item not found")
+    else :
+        return templates.TemplateResponse("interface.html", {"request": request,
                                                          "video_title": item["title"],
                                                          "page_title": item["title"],
                                                          "description": get_sources_hypertext(item["sources"])})
